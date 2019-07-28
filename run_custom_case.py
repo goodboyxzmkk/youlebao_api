9441c3632ec02eb8468471c4b01964aa46420555
ty <346315670@qq.com>
2019-07-28 21:17:45 +0800

    修改钉钉发送

--- run_custom_case.py
+++ run_custom_case.py
@@ -2,7 +2,7 @@ from test_interface.test_login_case import Test_Login_Case
 from test_interface.test_leaguer_level_case import Test_Leaguer_Level_Case
 from common import config_manage
 import unittest, HTMLTestRunner, time
-from common.send_dingding import send_ding,send_ding_to_url
+from common.send_dingding import send_ding,send_ding_url
 
 '''自定义运行测试用例'''
 login = unittest.TestLoader().loadTestsFromTestCase(Test_Login_Case)
@@ -17,10 +17,10 @@ if __name__ == '__main__':
     html_file = config_manage.REPORT_PATH + "TestResult_Report.html"  # 固定生成一份测试报告
     fp = open(html_file, "wb")
     '''retry，指定重试次数，如果save_last_try 为True ，一个用例仅显示最后一次测试的结果,为False，则显示所有重试的结果'''
-    runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='游乐宝_Web自动化测试报告', description='用例执行情况:', verbosity=2,
+    runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='游乐宝_接口自动化测试报告', description='用例执行情况:', verbosity=2,
                                            retry=0, save_last_try=False)
     runner.run(suites)
     fp.close()
     # send_ding_to_html("test")
-    send_ding("测试完成")
+    # send_ding("测试完成")
     # send_email.send_Email()
